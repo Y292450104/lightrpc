34a9ea33a875f110499601b9482b80735ffdadbb
yinaijie <yinaijie@yeah.net>
2018-09-01 08:58:55 +0800

    [YY] update light prc functions for source code read

--- src/main/java/rpc/xp/common/ResponseDecoder.java
+++ src/main/java/rpc/xp/common/ResponseDecoder.java
@@ -1,29 +1,33 @@
 package rpc.xp.common;
 
-import java.util.List;
-
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
 
+import java.util.List;
+
 public class ResponseDecoder extends ByteToMessageDecoder {
 
-	@Override
-	protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
-		// TODO Auto-generated method stub
-		if (in.readableBytes() < 4) {
-			return;
-		}
-		int index = in.readerIndex();
-		int readInt = in.readInt();
-		if (in.readableBytes() < readInt) {
-			in.readerIndex(index);
-			return;
-		}
-		byte[] bytes = new byte[readInt];
-		in.readBytes(bytes);
-		RpcResponse response = SerializationUtil.deserializeFromByte(bytes, RpcResponse.class);
-		out.add(response);
-	}
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        // 如果小于则说明是半包读取，需要等到下次在读取
+        if (in.readableBytes() < 4) {
+            return;
+        }
+
+        // 如果小于数据包的长度，则重置到最初，等待下次处理，半包处理
+        int index = in.readerIndex();
+        int readInt = in.readInt();
+        if (in.readableBytes() < readInt) {
+            in.readerIndex(index);
+            return;
+        }
+
+        // 读取当前包长度的字节数组，粘包的处理
+        byte[] bytes = new byte[readInt];
+        in.readBytes(bytes);
+        RpcResponse response = SerializationUtil.deserializeFromByte(bytes, RpcResponse.class);
+        out.add(response);
+    }
 
 }
--- src/main/java/rpc/xp/common/ResponseEncoder.java
+++ src/main/java/rpc/xp/common/ResponseEncoder.java
@@ -1,17 +1,40 @@
 package rpc.xp.common;
 
+import io.netty.buffer.AbstractReferenceCountedByteBuf;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundInvoker;
+import io.netty.channel.ChannelOutboundInvoker;
+import io.netty.channel.ChannelPipeline;
 import io.netty.handler.codec.MessageToByteEncoder;
+import sun.nio.ch.DirectBuffer;
 
 public class ResponseEncoder extends MessageToByteEncoder<RpcResponse> {
 
-	@Override
-	protected void encode(ChannelHandlerContext ctx, RpcResponse msg, ByteBuf out) throws Exception {
-		byte[] serializeToByte = SerializationUtil.serializeToByte(msg);
-		out.writeInt(serializeToByte.length);
-		out.writeBytes(serializeToByte);
+    @Override
+    protected void encode(ChannelHandlerContext ctx, RpcResponse msg, ByteBuf out) throws Exception {
+        byte[] serializeToByte = SerializationUtil.serializeToByte(msg);
+        out.writeInt(serializeToByte.length);
+        out.writeBytes(serializeToByte);
 
-	}
+
+        // 查看 cleaner 虚引用的使用
+        DirectBuffer directBuffer = null;
+        sun.misc.Cleaner cleaner = null;
+
+        // 引用计数
+        AbstractReferenceCountedByteBuf abstractReferenceCountedByteBuf = null;
+        ByteBuf byteBuf = null;
+
+        ChannelHandlerContext channelHandler = null;
+        ChannelInboundInvoker channelInboundInvoker = null;
+        ChannelOutboundInvoker channelOutboundInvoker = null;
+
+        // channelOutboundInvoker.write()
+        // ReferenceCountUtils.release(msg);
+        // Unpooled.buffer();
+
+        ChannelPipeline channelPipeline = null;
+    }
 
 }
